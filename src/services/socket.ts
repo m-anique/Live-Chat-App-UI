b94b358795cdbd5b5077f4c6d763acc11812abbf
M.AniqueDev <mohammadanique33@gmail.com>
2026-07-31 15:19:45 +0500

    Update socket.ts

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-export const SOCKET_URL = "http://localhost:5000";
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
 export function createSocket(token: string): Socket {
   return io(SOCKET_URL, {
